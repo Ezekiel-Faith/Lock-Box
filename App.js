af5a39f4c0d9998670c5fb7bab2451360759aedf
Ezekiel-Faith <ezekielfaith2000@gmail.com>
2025-10-07 04:59:01 +0100

    added logout functionality

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import WelcomeScreen from './screens/WelcomeScreen';
 import { Colors } from './constants/styles';
 import AuthContextProvider, { AuthContext } from './store/auth-context';
 import { useContext } from 'react';
+import IconButton from './components/ui/IconButton';
 
 const Stack = createNativeStackNavigator();
 
@@ -27,6 +28,12 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const authCtx = useContext(AuthContext);
+
+  function logoutHandler() {
+    authCtx.logout();
+  }
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -35,7 +42,20 @@ function AuthenticatedStack() {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name='Welcome' component={WelcomeScreen} />
+      <Stack.Screen
+        name='Welcome'
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon='exit'
+              Color={tintColor}
+              size={24}
+              onPress={logoutHandler}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
